efb85fd3acfdebef862da983ed9a6fb53afa7cc6
AdeshyaCG <adeshya.singhal.cg@gmail.com>
2026-09-22 13:58:39 +0530

    Added Topic 1 of Assignment 3

---
+++ Python/Assignment02.py
@@ -0,0 +1,114 @@
+# Q1
+age = "25"
+converted_age = int(age)
+print(converted_age,type(converted_age))
+
+# Q2
+marks = "75.5"
+converted_marks = float(marks)
+print(converted_marks,type(converted_marks))
+
+#Q3
+number = 50
+converted_number = float(number)
+print(converted_number,type(converted_number))
+
+#Q4
+marks2 = 85.9
+converted_marks2 = int(marks2)
+print(converted_marks2,type(converted_marks2))
+
+#Q5
+roll_number = 101
+converted_roll_number = str(roll_number)
+print(converted_roll_number,type(converted_roll_number))
+
+#Q6
+value1 = "18"
+value2 = "92.5"
+value3 = 100
+value4 = 45.8
+
+value1 = int(value1)
+value2 = float(value2)
+value3 = str(value3)
+value4 = int(value4)
+
+print(value1, type(value1))
+print(value2, type(value2))
+print(value3, type(value3))
+print(value4, type(value4))
+
+#Q7
+a = "20"
+b = int(a)
+
+c = 10.8
+d = int(c)
+
+e = 25
+f = str(e)
+
+print(b)
+print(d)
+print(f)
+print(type(b))
+print(type(d))
+print(type(f))
+
+#Q8
+age = "19"
+new_age = age + 1
+
+#correct code
+
+age = "19"
+new_age = int(age) + 1
+print("Age:", new_age)
+
+#Q9
+marks = "85"
+
+marks = int(marks)
+
+final_marks = marks + 5
+
+print("Final Marks:", final_marks)
+
+#Q10
+price = "1499.50"
+
+price = float(price)
+
+total_amount = price + 99.50
+
+print("Total Amount:", total_amount)
+
+#Q11
+a = 20
+b = 6
+
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a//b
+h = a%b
+i = a**b
+
+#Q12
+# a = 17
+# b = 5
+
+# print(a / b) = 3.4
+# print(a // b) = 3
+# print(a % b) = 2
+
+# /gives the full division an // gives only a integer vlue & % gives the remender
+
+#Q13
+# result = 10 + 5 * 2
+# print(result) = 20
+
+
+
